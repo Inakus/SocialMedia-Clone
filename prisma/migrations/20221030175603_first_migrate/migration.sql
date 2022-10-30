5b06360ff69e48a934e5e2c2b6ecd24e42aeb479
Karol Dobrek <Inakus@wp.pl>
2022-10-30 18:56:15 +0100

    change prisma schema

--- prisma/migrations/20221030175603_first_migrate/migration.sql
+++ prisma/migrations/20221030175603_first_migrate/migration.sql
@@ -45,10 +45,12 @@ CREATE TABLE "Comment" (
 CREATE TABLE "Like" (
     "userId" TEXT NOT NULL,
     "commentId" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
 
-    PRIMARY KEY ("userId", "commentId"),
+    PRIMARY KEY ("userId", "commentId", "postId"),
     CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Like_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Like_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
